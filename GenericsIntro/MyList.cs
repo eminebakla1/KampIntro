e63b9d1510918aa426d611099cbe95cf71c6538d
Ekin İnci <ekin_-_inci@outlook.com>
2021-02-07 01:31:26 +0300

    07.02.2021

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] İtems; //metodun dışında class'ın içinde 
+
+      public void Add(T item)
+        {
+
+        }
+
+    }
+}
