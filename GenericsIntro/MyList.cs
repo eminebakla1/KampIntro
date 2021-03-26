e91c88560e5fb72a956c608012f1014b290df435
Ekin İnci <ekin_-_inci@outlook.com>
2021-03-26 22:52:18 +0300

    ....

--- GenericsIntro/MyList.cs
+++ GenericsIntro/MyList.cs
@@ -7,9 +7,21 @@ namespace GenericsIntro
     class MyList<T>
     {
         T[] İtems; //metodun dışında class'ın içinde 
-
+        //constructor
+        public MyList()
+        {
+            İtems = new T[0];
+        }
       public void Add(T item)
         {
+            T[] tempArray = İtems;//tempArray geçici dizi demek
+            İtems = new T[İtems.Length+1];
+            for (int i = 0; i <tempArray.Length; i++)
+            {
+                İtems[i] = tempArray[i];
+            }
+
+            İtems[İtems.Length - 1]=item;
 
         }
 
