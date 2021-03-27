2ca881ea2c5d04ee6cdea08b44461de2567ff0f1
Ekin İnci <ekin_-_inci@outlook.com>
2021-03-27 10:50:14 +0300

    ..

--- OOP2/Program.cs
+++ OOP2/Program.cs
@@ -9,25 +9,33 @@ namespace OOP2
             GercekMusteri musteri1 = new GercekMusteri();
             musteri1.Id = 1;
             musteri1.MusteriNo = "12345";
-            musteri1.Adi = "Engin";
-            musteri1.Soyadi = "Demiroğ";
+            musteri1.Adi = "Emine";
+            musteri1.Soyadi = "Bakla";
             musteri1.TcNo = "12345678910";
 
             //Kodlama.io
 
-            TüzelMüsteri müsteri2 = new TüzelMüsteri();
-            müsteri2.Id = 2;
-            müsteri2.MusteriNo = "54321";
-            müsteri2.SirketAdi = "Kodlama.io";
-            müsteri2.VergiNo = "1234567890";
+            TüzelMüsteri musteri2 = new TüzelMüsteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "1234567890";
 
 
 
-            
             //Gerçek Müşteri-Tüzel Müşteri
             //SOLID
 
-            //51.31 5. ders
+            Musteri musteri3 = new GercekMusteri();//bir yerde new görürsen bu bellekteki referans nosu
+            Musteri musteri4 = new TüzelMüsteri();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(musteri1);
+            customerManager.Add(musteri2);
+
+            
+
+            
         }
     }
 }
